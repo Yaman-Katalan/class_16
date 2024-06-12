4b636040422624fd3515c948f76ec101b23a4582
Yaman-Katalan <23040456@student.ltuc.com>
2024-06-12 13:52:13 +0000

    abc

---
+++ api/index.py
@@ -0,0 +1,15 @@
+# Implementing Serverless Function:
+
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        msg = "welcome from the future!"
+        self.wfile.write(msg.encode())
+
+        return
